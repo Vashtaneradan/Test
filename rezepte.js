5c50ccd3973e3f86098dbaa0881f61484edda238
Vashtaneradan <andrea@feurer-net.de>
2017-04-02 10:28:28 +0200

    outsourcing the cocktail-list to a new file(rezepte.js), moved old html to backup file

---
+++ rezepte.js
@@ -0,0 +1,241 @@
+    var cocktails_ingredients = [
+
+    //Acapulco
+    {
+        id_cocktail: 1,
+        id_ingredients: 1,
+        amount: "3 cl",
+    },
+    {
+        id_cocktail: 1,
+        id_ingredients: 2,
+        amount: "1 cl",
+    },
+    {
+        id_cocktail: 1,
+        id_ingredients: 3,
+        amount: "4 cl",
+    },
+    {
+        id_cocktail: 1,
+        id_ingredients: 4,
+        amount: "10 cl",
+    },
+
+    //Azzuro Baccio
+    {
+        id_cocktail: 2,
+        id_ingredients: 5,
+        amount: "3 cl",
+    },
+    {
+        id_cocktail: 2,
+        id_ingredients: 6,
+        amount: "2 cl",
+    },
+    {
+        id_cocktail: 2,
+        id_ingredients: 7,
+        amount: "1 cl",
+    },
+    {
+        id_cocktail: 2,
+        id_ingredients: 8,
+        amount: "10 cl",
+    },
+
+    //Bahama Mama
+    {
+        id_cocktail: 3,
+        id_ingredients: 2,
+        amount: "2 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 9,
+        amount: "1 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 10,
+        amount: "2 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 11,
+        amount: "-",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 4,
+        amount: "12 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 7,
+        amount: "2 cl",
+    },
+    //Black Death
+    {
+        id_cocktail: 4,
+        id_ingredients: 5,
+        amount: "4 cl",
+    },
+    {
+        id_cocktail: 4,
+        id_ingredients: 1,// welcher tequila
+        amount: "2 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 12,
+        amount: "2 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 7,
+        amount: "4 cl",
+    },
+    {
+        id_cocktail: 3,
+        id_ingredients: 13,
+        amount: "8 cl",
+    },
+    //Cosmopolitan
+    {
+        id_cocktail: 4,
+        id_ingredients: 14,
+        amount: "3 cl",
+    },
+    {
+        id_cocktail: 4,
+        id_ingredients: 15,
+        amount: "6 cl",
+    },
+    {
+        id_cocktail: 4,
+        id_ingredients: 16,
+        amount: "3 cl",
+    },
+    {
+        id_cocktail: 4,
+        id_ingredients: 6,
+        amount: "1 1/2 cl",
+    },
+        
+    ];
+    var ingredients = [
+    {
+        id: 1,
+        name: "Tequila Silver",
+        vol: true,
+    },
+    {
+        id: 2,
+        name: "brauner Rum",
+        vol: true,
+    },
+    {
+        id: 3,
+        name: "Grapefruitsaft",
+        vol: false,
+    },
+    {
+        id: 4,
+        name: "Ananassaft",
+        vol: false,
+    },
+    {
+        id: 5,
+        name: "Blue Curacao",
+        vol: false,
+    },
+    {
+        id: 6,
+        name: "Limettensirup/Saft",
+        vol: false,
+    },
+    {
+        id: 7,
+        name: "Zitronensaft",
+        vol: false,
+    },
+    {
+        id: 8,
+        name: "Sekt(kalt)",
+        vol: false,
+    },
+    {
+        id: 9,
+        name: "Jamaika Rum 73 %vol",
+        vol: false,
+    },
+    {
+        id: 10,
+        name: "Malibu Coconut",
+        vol: false,
+    },
+    {
+        id: 11,
+        name: "Kahula Coffee Liquer",
+        vol: false,
+    },
+    {
+        id: 12,
+        name: "Grenadine",
+        vol: false,
+    },
+    {
+        id: 13,
+        name: "Blutorange",
+        vol: false,
+    },
+    {
+        id: 14,
+        name: "Cointreau",
+        vol: false,
+    },
+    {
+        id: 15,
+        name: "Wodka",
+        vol: false,
+    },
+    {
+        id: 16,
+        name: "Cranberrynektar",
+        vol: false,
+    },
+
+    ];
+var cocktails = [
+    {
+        id: 1,
+        name: "Acapulco ",
+        how_to: "Mit Eis im Shaker shaken und anschießend in ein Longdrinkglas abgießen."
+
+    },
+    {
+        id: 2,
+        name: "Azzuro Baccio ",
+        how_to: "ohne Sekt mit Eis im Shaker shaken, durch Barsieb in ein großes Stielglas abgießen. Mit kaltem Sekt auffüllen."
+
+    },
+    {
+        id: 3,
+        name: "Bahama Mama ",
+        how_to: "Mit Eis im Shaker shaken und dann in ein Longdrinkglas auf Eiswürfel abgießen."
+
+    },
+    {
+        id: 4,
+        name: "Black Death ",
+        how_to: "Mit Eis im Shaker shaken und dann in ein Longdrinkglas auf Eiswürfel abgießen."
+
+    },
+    {
+        id: 5,
+        name: "Cosmopolitan ",
+        how_to: "Mit Eis im Shaker shaken und in ein gekühltes Cocktailglas abgießen."
+
+    },
+    ];
